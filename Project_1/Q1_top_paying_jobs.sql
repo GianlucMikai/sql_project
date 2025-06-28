06e43f157f8e6770a0b029b6d1083b7ee0c9acd9
Gianluc Mikai Robinson <97705595+GianlucMikai@users.noreply.github.com>
2025-06-28 17:10:43 -0400

    Complete

--- Project_1/Q1_top_paying_jobs.sql
+++ Project_1/Q1_top_paying_jobs.sql
@@ -16,19 +16,30 @@ SELECT *
 FROM skills_job_dim
 LIMIT 10*/
 
-
+/*
+QUESTIONS: What are the TOP paying data analyst jobs
+- Identify the top 10 highest paying Data analyst jobs
+- Only job postings with salaries (No Nulls)
+- Why? Highlight the top-paying opportunities for data analysts, offering insights into employment skills needed etc.
+*/
 
 SELECT
   job_id,
   job_title,
   job_location,
-  salary_year_avg
+  salary_year_avg,
+  job_posted_date,
+  name AS company_name
 FROM 
   job_postings_fact
+
+LEFT JOIN 
+  company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE
   job_title_short = 'Data Analyst' AND
   job_location = 'Anywhere' AND
   salary_year_avg IS NOT NULL
+ORDER BY salary_year_avg DESC
 LIMIT 10
 
 
