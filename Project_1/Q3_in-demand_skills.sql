25779734538b9a89c3422191dc9889015058b584
Gianluc Mikai Robinson <97705595+GianlucMikai@users.noreply.github.com>
2025-07-21 17:38:37 -0400

    Query 3 complete

---
+++ Project_1/Q3_in-demand_skills.sql
@@ -0,0 +1,21 @@
+/*
+
+What are the most in-demand skills for my role?
+-join job postings tables to inner join table similar to query 2
+- identify the top 5 in-demand skills for a data analyst
+-focus on all job postings
+*/
+
+SELECT 
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN   skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+    job_title_short = 'Data Analyst'
+GROUP BY
+    skills
+ORDER BY
+    demand_count DESC
+LIMIT 5 
